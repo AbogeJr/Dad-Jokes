4847201d85f41c15890e22076c9eb5103fe5f9b0
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-04-04 11:47:12 +0300

    update: fixed crash

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// import axios from 'https://unpkg.com/axios/dist/axios.min.js'
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 const themeBtn = document.getElementById('theme-btn')
 const body = document.getElementById('body')
-const axios = 'axios'
+
 // body.classList.add('dark')
 
 jokeBtn.addEventListener('click', generateJoke)
@@ -11,27 +10,15 @@ jokeBtn.addEventListener('click', generateJoke)
 generateJoke()
 
 async function generateJoke() {
-
-    
-
-    const res = await axios.get('https://icanhazdadjoke.com/', {
+    const config = {        //headers required by API
         headers: {
-            Accept: 'application/json'
-        }
-    });
+            Accept: 'application/json',
+        },
+    }
+    
+    const res = await fetch('https://icanhazdadjoke.com/', config)      //returns promise
     const data = await res.json()           //Parse data into json
     jokeEl.innerHTML = data.joke            //Displays joke on page
-
-
-    // const config = {        //headers required by API
-    //     headers: {
-    //         Accept: 'application/json',
-    //     },
-    // }
-    
-    // const res = await fetch('https://icanhazdadjoke.com/', config)      //returns promise
-    // const data = await res.json()           //Parse data into json
-    // jokeEl.innerHTML = data.joke            //Displays joke on page
 }
 
 // Dark mode toggler
